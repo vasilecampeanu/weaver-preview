1c6774ee1f4e5e3f0a556f03c31f058076d97e37
vasilecampeanu <vasilecampeanu.link@yahoo.com>
2023-09-03 23:18:08 +0300

    feat: Cleaned up the project and added support for scss.

---
+++ src/settings.ts
@@ -0,0 +1,21 @@
+import { App, PluginSettingTab } from "obsidian";
+import Weaver from "main";
+
+import { WeaverSettings } from "interfaces/WeaverSettings";
+
+export const DEFAULT_SETTINGS: WeaverSettings = {}
+
+export default class SampleSettingTab extends PluginSettingTab {
+	private readonly plugin: Weaver;
+
+	constructor(app: App, plugin: Weaver) {
+		super(app, plugin);
+		this.plugin = plugin;
+	}
+
+	display(): void {
+		const { containerEl } = this;
+		containerEl.empty();
+	}
+}
+
