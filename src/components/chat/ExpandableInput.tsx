231ac593e45b5de3827898d09d2354a430c3913b
Vasile Campeanu <# .env binding to m file#v.campeanu@topmotive.eu>
2023-09-13 17:22:08 +0300

    feat: Added new animation when showing the char count info bar.

--- src/components/chat/ExpandableInput.tsx
+++ src/components/chat/ExpandableInput.tsx
@@ -67,16 +67,15 @@ export const ExpandableInput: React.FC<ExpandableInputProps> = ({ leftDivWidth, 
 			}}
 			onHoverEnd={() => {
 				isHovering.current = false;
-			
+
 				if (textValue.length > 0) return;
-			
+
 				if (!isFocused.current && !isPinned) {
 					borderRadiusControls.start({ borderRadius: "20px" });
 					heightControls.start({ height: "auto" });
 					setShowCount(false);
 				}
 			}}
-			
 			onAnimationComplete={() => {
 				if (isHovering.current) {
 					setShowCount(true);
@@ -97,15 +96,15 @@ export const ExpandableInput: React.FC<ExpandableInputProps> = ({ leftDivWidth, 
 							}}
 							onBlur={() => {
 								isFocused.current = false;
-							
+
 								if (textValue.length > 0) return;
-							
+
 								if (!isHovering.current && !isPinned) {
 									borderRadiusControls.start({ borderRadius: "20px" });
 									heightControls.start({ height: "auto" });
 									setShowCount(false);
 								}
-							}}							
+							}}
 						/>
 						<div className="ow-user-actions">
 							<button
@@ -125,7 +124,13 @@ export const ExpandableInput: React.FC<ExpandableInputProps> = ({ leftDivWidth, 
 						</div>
 					</div>
 					{showCount && (
-						<div className="ow-chracters-count">
+						<motion.div
+							className="ow-chracters-count"
+							initial={{ opacity: 0 }}
+							animate={{ opacity: 1 }}
+							exit={{ opacity: 0 }}
+							transition={{ duration: 0.2 }}
+						>
 							<div className="ow-count">
 								<span className="ow-chat-count">{charCount}/2000</span>
 								<span className="ow-token-count">{tokenCount}</span>
@@ -145,7 +150,7 @@ export const ExpandableInput: React.FC<ExpandableInputProps> = ({ leftDivWidth, 
 									<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-pin"><line x1="12" x2="12" y1="17" y2="22" /><path d="M5 17h14v-1.76a2 2 0 0 0-1.11-1.79l-1.78-.9A2 2 0 0 1 15 10.76V6h1a2 2 0 0 0 0-4H8a2 2 0 0 0 0 4h1v4.76a2 2 0 0 1-1.11 1.79l-1.78.9A2 2 0 0 0 5 15.24Z" /></svg>
 								</button>
 							</div>
-						</div>
+						</motion.div>
 					)}
 				</div>
 			</div>
