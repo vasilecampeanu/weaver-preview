59ed32750181d7bddca1917eaab085018f5abfd1
Vasile Campeanu <# .env binding to m file#v.campeanu@topmotive.eu>
2023-09-13 10:11:30 +0300

    feat: Added new chat btn.

--- src/components/chat/Chat.tsx
+++ src/components/chat/Chat.tsx
@@ -137,7 +137,6 @@ const ExpandableInput: React.FC<ExpandableInputProps> = ({ leftDivWidth, heightC
 	);
 };
 
-
 export const InputWrapper: React.FC = () => {
 	const [leftDivWidth, setLeftDivWidth] = useState<number | null>(null);
 	const leftDivRef = useRef<HTMLDivElement | null>(null);
@@ -161,7 +160,11 @@ export const InputWrapper: React.FC = () => {
 			transition={{ duration: 0.3 }}
 			className="ow-input-wrapper"
 		>
-			<div ref={leftDivRef} className="ow-input-left">Left Content</div>
+			<div ref={leftDivRef} className="ow-input-left">
+				<button className="ow-new-chat">
+					<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-plus"><path d="M5 12h14"/><path d="M12 5v14"/></svg>
+				</button>
+			</div>
 			{leftDivWidth !== null && <ExpandableInput leftDivWidth={leftDivWidth} heightControls={heightControls} />}
 		</motion.div>
 	);
